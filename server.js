6e67d77cfee630984fdd5e1df27b4d9c56d2b141
JMWilliamsPC <jarrett0204@gmail.com>
2021-02-17 01:57:14 -0500

    changed server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const path = require("path");
+//const path = require("path");//
+const app = express();
+const PORT = process.env.PORT || 3000;
 const fs = require("fs");
 
-const app = express();
-const PORT = 3000;
+
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -57,4 +58,4 @@ app.delete("/api/notes/:id", function(req, res){
 })*/
 app.listen(PORT, function(){
     console.log("server is listening on: http://localhost:" + PORT )
-})
+});
